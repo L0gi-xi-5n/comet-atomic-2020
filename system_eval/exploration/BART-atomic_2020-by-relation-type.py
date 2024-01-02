9eda5d946f1b2cb7c0d7f73a08862ecf64447a52
Fred Lefever <fred.lefever@kuleuven.be>
2024-01-02 09:44:29 +0100

    Minor improvement and cleanup

--- system_eval/exploration/BART-atomic_2020-by-relation-type.py
+++ system_eval/exploration/BART-atomic_2020-by-relation-type.py
@@ -5,8 +5,6 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import json
 
-from LaTeX_utils import set_size
-
 os.chdir("partition-by-relation")
 
 df=pd.read_json('../../BART/BART-atomic_2020.json', lines=True)
@@ -15,5 +13,5 @@ grouped_by_relation = df.groupby(df.relation)
 for relation in set(grouped_by_relation.keys):
     outfile_path = os.path.join(os.path.basename(relation).split('.')[0] + ".json")
     outfile = open(outfile_path, 'w')
-    df_new = grouped_by_relation.get_group(relation)
-    df_new.apply(lambda row: outfile.write(row.to_json() + "\n"), axis=1)
+    grouped_by_relation.get_group(relation).apply(lambda row: outfile.write(row.to_json() + "\n"), axis=1)
+
