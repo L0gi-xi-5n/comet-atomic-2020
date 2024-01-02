a35cfc0271a3535793b25b314cef61b8bc83b71b
Fred Lefever <fred.lefever@kuleuven.be>
2024-01-02 09:36:17 +0100

    First partitioning of BART-atomic_2020 by relation

--- system_eval/exploration/BART-atomic_2020-by-relation-type.py
+++ system_eval/exploration/BART-atomic_2020-by-relation-type.py
@@ -1,13 +1,19 @@
+import os
 import pandas as pd
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import json
 
 from LaTeX_utils import set_size
 
+os.chdir("partition-by-relation")
 
-df=pd.read_json('../BART/BART-atomic_2020.json', lines=True)
+df=pd.read_json('../../BART/BART-atomic_2020.json', lines=True)
 
-grouped_by_relation=df.groupby(df.relation)
-df_new=grouped_by_relation.get_group("xNeed")
-
+grouped_by_relation = df.groupby(df.relation) 
+for relation in set(grouped_by_relation.keys):
+    outfile_path = os.path.join(os.path.basename(relation).split('.')[0] + ".json")
+    outfile = open(outfile_path, 'w')
+    df_new = grouped_by_relation.get_group(relation)
+    df_new.apply(lambda row: outfile.write(row.to_json() + "\n"), axis=1)
