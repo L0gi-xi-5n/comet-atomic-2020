b86e1ba148b7bc7426ee0c36fd8212d1761c11e8
Fred Lefever <fred.lefever@kuleuven.be>
2024-01-02 14:52:10 +0100

    First implementation of automatic evaluation of by-relation parts

--- system_eval/exploration/BART-atomic_2020-by-relation-type.py
+++ system_eval/exploration/BART-atomic_2020-by-relation-type.py
@@ -1,22 +1,37 @@
 import os
+import sys
 import pandas as pd
 
-out_dir_name = "partition-by-relation"
+sys.path.append("../system_eval")
+from system_eval.automatic_eval_exploration import eval_partition
+
+in_file_name = os.path.join("BART", "BART-atomic_2020.json")
+out_dir_name = os.path.join("exploration", "partition-by-relation")
 
 def create_dir(dir_name):
-    out_dir = os.path.dirname(os.getcwd() + "/" + dir_name + "/")
+    out_dir = os.path.dirname(os.path.join(os.getcwd(), dir_name) + os.path.sep)
     out_dir_exists = os.path.exists(out_dir)
     if not out_dir_exists:
+        print("Creating " + out_dir_name)
         os.makedirs(out_dir)
+    else:
+        print("Output directory already exists")
+
+def partition_by_relation(data):
+    grouped_by_relation = data.groupby(data.relation) 
+    for relation in set(grouped_by_relation.keys):
+        outfile_path = os.path.join(out_dir_name, os.path.basename(relation).split('.')[0] + ".json")
+        outfile = open(outfile_path, 'w')
+        grouped_by_relation.get_group(relation).apply(lambda row: outfile.write(row.to_json() + "\n"), axis=1)
+
 
 create_dir(out_dir_name)
-os.chdir(out_dir_name)
 
-df=pd.read_json('../../BART/BART-atomic_2020.json', lines=True)
+df = pd.read_json(in_file_name, lines=True)
+partition_by_relation(df)
 
-grouped_by_relation = df.groupby(df.relation) 
-for relation in set(grouped_by_relation.keys):
-    outfile_path = os.path.join(os.path.basename(relation).split('.')[0] + ".json")
-    outfile = open(outfile_path, 'w')
-    grouped_by_relation.get_group(relation).apply(lambda row: outfile.write(row.to_json() + "\n"), axis=1)
-
+for file_name in os.listdir(out_dir_name):
+    print("Evaluating " + file_name)
+    evaluated = os.path.join("exploration", "partition-by-relation", file_name)
+    evaluation = eval_partition(evaluated)
+    print(evaluation)
